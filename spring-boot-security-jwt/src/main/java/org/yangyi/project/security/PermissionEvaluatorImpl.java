1b4de125c9c3c089cc8443dd864786638275a7ea
杨毅 <876359828@qq.com>
2023-04-03 10:00:30 +0800

    功能添加

---
+++ spring-boot-security-jwt/src/main/java/org/yangyi/project/security/PermissionEvaluatorImpl.java
@@ -0,0 +1,50 @@
+package org.yangyi.project.security;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.yangyi.project.system.po.SysMenu;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * hasPermission()的注解会委托给PermissionEvaluator接口，而这个接口默认实现是DenyAllPermissionEvaluator
+ * 这个类的两个方法直接返回了false，所以我们必须自己实现一个PermissionEvaluator 并且把它注入到默认DefaultWebSecurityExpressionHandler类中
+ * （注意与DefaultMethodSecurityExpressionHandler区分，这两个类都持有permissionEvaluator 应该一个是web环境一个是SE环境,我这里是web环境，故而是DefaultWebSecurityExpressionHandler类）
+ */
+@Component
+public class PermissionEvaluatorImpl implements PermissionEvaluator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PermissionEvaluatorImpl.class);
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+        LOGGER.info("请求：[{}]，需要权限：[{}]", targetDomainObject, permission);
+        User user = (User) authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+        for (GrantedAuthority authority : authorities) {
+            List<SysMenu> sysMenus = ((UrlGrantedAuthority) authority).getSysMenus();
+            for (SysMenu sysMenu : sysMenus) {
+//                if (antPathMatcher.match(sysMenu.getMenuUrl(), (String) targetDomainObject) &&
+//                        (StringUtils.equals(sysMenu.getMenuTag(), (String) permission) || StringUtils.equals(sysMenu.getMenuTag(), "*")))
+//                    return true;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        return true;
+    }
+
+}
---
+++ spring-boot-security-jwt/src/main/java/org/yangyi/project/system/vo/SysUserVO.java
@@ -0,0 +1,160 @@
+package org.yangyi.project.system.vo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SysUserVO {
+
+    private Long userId;
+
+    private String userName;
+
+    private String nickName;
+
+    private String email;
+
+    private String phone;
+
+    private String sex;
+
+    private String avatar;
+
+    private String password;
+
+    private String status;
+
+    private String delFlag;
+
+    private String createBy;
+
+    private LocalDateTime createTime;
+
+    private String updateBy;
+
+    private LocalDateTime updateTime;
+
+    private String remark;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
