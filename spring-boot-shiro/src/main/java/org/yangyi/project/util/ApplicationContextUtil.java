c47421d3a503e798f84b5c550461e57382b0557d
杨毅 <876359828@qq.com>
2024-11-28 17:24:30 +0800

    功能新增

--- spring-boot-shiro/src/main/java/org/yangyi/project/util/ApplicationContextUtil.java
+++ spring-boot-shiro/src/main/java/org/yangyi/project/util/ApplicationContextUtil.java
@@ -3,20 +3,27 @@ package org.yangyi.project.util;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationContextUtil implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
+    private static Environment environment;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         ApplicationContextUtil.applicationContext = applicationContext;
+        ApplicationContextUtil.environment = applicationContext.getEnvironment();
     }
 
     public static ApplicationContext getApplicationContext() {
-        return applicationContext;
+        return ApplicationContextUtil.applicationContext;
+    }
+
+    public static Environment getEnvironment() {
+        return ApplicationContextUtil.environment;
     }
 
     public static Object getBean(String beanName) {
@@ -29,6 +36,22 @@ public class ApplicationContextUtil implements ApplicationContextAware {
 
     public static <T> T getBean(String beanName, Class<T> clazz) {
         return getApplicationContext().getBean(beanName, clazz);
+    }
+
+    public static String getProperty(String key) {
+        return getEnvironment().getProperty(key);
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+        return getEnvironment().getProperty(key, defaultValue);
+    }
+
+    public static <T> T getProperty(String key, Class<T> clazz) {
+        return getEnvironment().getProperty(key, clazz);
+    }
+
+    public static <T> T getProperty(String key, Class<T> clazz, T defaultValue) {
+        return getEnvironment().getProperty(key, clazz, defaultValue);
     }
 
 }
